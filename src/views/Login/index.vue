03e0213d88c8c9f73c45a5445b46bb1ff2402f50
justyouso <1242646507@qq.com>
2020-02-16 15:50:38 +0800

    feat: 完成登录页面

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -5,27 +5,31 @@
         <li v-for="item in menuTab" :key="item.id" :class="{'current': item.current}" @click="toggleMenu(item)">{{item.txt}}</li>
       </ul>
       <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="login-form" size="medium">
-        <el-form-item prop="pass" class="item-form">
+        <el-form-item prop="username" class="item-form">
           <label>邮箱</label>
-          <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+          <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
         </el-form-item>
 
-        <el-form-item prop="checkPass" class="item-form">
+        <el-form-item prop="password" class="item-form">
           <label>密码</label>
-          <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+          <el-input type="text" v-model="ruleForm.password" autocomplete="off" minlength="6" maxlength="20"></el-input>
         </el-form-item>
 
-        <el-form-item prop="age" class="item-form">
+        <el-form-item prop="passwords" class="item-form" v-show="model === 'register'">
+          <label>重复密码</label>
+          <el-input type="text" v-model="ruleForm.passwords" autocomplete="off" minlength="6" maxlength="20"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="code" class="item-form">
           <label>验证码</label>
           <el-row :gutter="10">
             <el-col :span="15">
-               <el-input v-model.number="ruleForm.age"></el-input>
+               <el-input v-model.number="ruleForm.code"></el-input>
             </el-col>
             <el-col :span="9"> 
               <el-button type="success" class="block">获取验证码</el-button>
             </el-col>
           </el-row> 
-         
         </el-form-item>
 
         <el-form-item>
@@ -37,64 +41,99 @@
 </template>
 
 <script>
+import { stripscript, validateEmailPwdCode } from '@/utils/validate'
 export default {
   name: "login",
   data(){
-    var checkAge = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('年龄不能为空'));
-        }
-        setTimeout(() => {
-          if (!Number.isInteger(value)) {
-            callback(new Error('请输入数字值'));
-          } else {
-            if (value < 18) {
-              callback(new Error('必须年满18岁'));
-            } else {
-              callback();
-            }
-          }
-        }, 1000);
-      };
-      var validatePass = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入密码'));
-        } else {
-          if (this.ruleForm.checkPass !== '') {
-            this.$refs.ruleForm.validateField('checkPass');
-          }
-          callback();
-        }
-      };
-      var validatePass2 = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请再次输入密码'));
-        } else if (value !== this.ruleForm.pass) {
-          callback(new Error('两次输入密码不一致!'));
-        } else {
-          callback();
-        }
-      };
+    //用户名验证
+    var validateUsername = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入用户名'));
+      } else if (!validateEmailPwdCode(value,'email')){ // 验证邮箱各格式
+        callback(new Error('邮箱格式错误'))
+      }else{
+        callback();
+      }
+    };
+
+    //密码验证
+    var validatePassword = (rule, value, callback) => {
+      //过滤掉特殊字符
+      // 重新绑定form表单中的password
+      this.ruleForm.password = stripscript(value)
+      // 将过滤的字符重新赋值给value
+      value = stripscript(value)
+      if (!value) {
+        return callback(new Error('请输入密码'));
+      }else if (!validateEmailPwdCode(value,'pwd')){ //验证密码
+        callback(new Error('密码为6至20位数字+字母'))
+      }else{
+        callback();
+      }
+    };
+
+    //重复密码验证
+    var validatePasswords = (rule, value, callback) => {
+      // 如果模块值为login,则直接通过,因为login没有重复密码
+      if (this.model=='login') {callback();}
+
+      //过滤掉特殊字符
+      // 重新绑定form表单中的passwords
+      this.ruleForm.passwords = stripscript(value)
+      // 将过滤的字符重新赋值给value
+      value = stripscript(value)
+      if (!value) {
+        return callback(new Error('请输入密码'));
+      }else if (value != this.ruleForm.password){ //验证重复密码
+        callback(new Error('重复密码错误'))
+      }else{
+        callback();
+      }
+    };
+    //验证码验证
+    var validateCode = (rule, value, callback) => {
+      //过滤掉特殊字符
+      // 重新绑定form表单中的code
+      this.ruleForm.code = stripscript(value)
+      // 将过滤的字符重新赋值给value
+      value = stripscript(value)
+      if (value === '') {
+        callback(new Error('请输入验证码'));
+      } else if (!validateEmailPwdCode(value,'code')) {
+        callback(new Error('验证码格式错误'));
+      } else {
+        callback();
+      }
+    };
     return {
+      //模块值
+      model:'login',
+      //表单数据
       ruleForm: {
-          pass: '',
-          checkPass: '',
-          age: ''
+          username: '',
+          password: '',
+          passwords:'',
+          code: ''
         },
+      // 验证规则
       rules: {
-        pass: [
-          { validator: validatePass, trigger: 'blur' }
+        username: [
+          { validator: validateUsername, trigger: 'blur' }
         ],
-        checkPass: [
-          { validator: validatePass2, trigger: 'blur' }
+        password: [
+          { validator: validatePassword, trigger: 'blur' }
         ],
-        age: [
-          { validator: checkAge, trigger: 'blur' }
+        passwords: [
+          { validator: validatePasswords, trigger: 'blur' }
+        ],
+        code: [
+          { validator: validateCode, trigger: 'blur' }
         ]
       },
+      // 菜单表
       menuTab:[
-        {txt: "登陆","current":true},
-        {txt: "注册","current":false}
+        {txt: "登陆","current":true,"type":'login'},
+        {txt: "注册","current":false,"type":'register'}
       ]
     }
   },
@@ -109,6 +148,7 @@ export default {
       });
       //添加高光
       data.current=true
+      this.model =data.type
     },
     // 提交
     submitForm(formName) {
